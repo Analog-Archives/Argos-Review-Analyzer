9845c49e51520d239a7d68d2657d247d8ac6db4c
Vihan Perera <vihanpereraux@gmail.com>
2024-07-27 03:12:45 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>App Works</h1>
+      <Button>Test Button</Button>
+    </>
+  );
+}
