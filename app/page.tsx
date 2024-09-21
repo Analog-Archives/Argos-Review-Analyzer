db7ab6fe1079086c5305a799660dec7f5c053738
Vihan Perera <vihanpereraux@gmail.com>
2024-09-21 16:20:40 +0100

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,25 @@
 
 import Image from "next/image";
 
+// bootstrap components
+import Container from 'react-bootstrap/Container';
+
 // components
 import SearchSection from "@/components/app/home/search-section/search-section";
 import DisclaimerText from "@/components/app/home/disclaimer-section/disclaimer";
+import TopNavbar from "@/components/app/navbar/navbar";
 
 export default function Home() {
   return (
-    <div className="container">
+    <Container>
       {/* navbar */}
+      <TopNavbar />
 
       {/* search section */}
       <SearchSection />
 
       {/* disclaimer text */}
       <DisclaimerText />
-    </div>
+    </Container>
   );
 }
