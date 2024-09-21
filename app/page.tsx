3f144d6706839a2d7070863def5516dcb8bfb06f
Vihan Perera <vihanpereraux@gmail.com>
2024-09-21 15:27:36 +0100

    add search section

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import SearchSection from "@/components/app/home/search-section/search-section";
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
       {/* navbar */}
 
       {/* search section */}
       <SearchSection />
 
       {/* disclaimer text */}
-    </>
+    </div>
   );
 }
--- components/app/home/search-section/search-section.tsx
+++ components/app/home/search-section/search-section.tsx
@@ -6,14 +6,23 @@ import { Input } from '@/components/ui/input'
 // shad cn components
 import { Button } from "@/components/ui/button";
 
+// stylesheet
+import Styles from './search-section.module.css'
+
 const SearchSection: React.FC<{}> = () => {
     return (
-        <>  
+        <div className={`${Styles.wrapper} mx-auto d-block`}>
             <p>Enter your product code here 👇</p>
-            <Input />
-            <Button />
+            <div style={{
+                display: 'flex',
+                marginTop: 20,
+                marginBottom: 10
+            }}>
+                <Input />
+                <Button>Search</Button>
+            </div>
             <a href="">How to get the product code ?</a>
-        </>
+        </div>
     )
 }
 
