16ccacc20832c67eb49828c285ac4dfdd6cd466f
Vihan Perera <vihanpereraux@gmail.com>
2024-09-22 09:40:12 +0100

    add result card

---
+++ components/app/home/result-card/result-card.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+// components
+import {
+    Card,
+    CardDescription,
+    CardFooter
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
+// interface
+import { ResultCardProps } from '@/interfaces/Result-Card-Interface'
+
+// stylesheet
+import Styles from './result-card.module.css'
+
+const ResultCard: React.FC<ResultCardProps> = ({ title }) => {
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 50
+        }}>
+            <Card className={Styles.wrapper}>
+                <CardDescription
+                    style={{
+                        color: 'white',
+                        fontWeight: 500,
+                        fontSize: 14,
+                        textTransform: 'capitalize'
+                    }}>
+                    {title}</CardDescription>
+
+                <hr style={{ color: '#E9E9E9' }} />
+
+                <CardFooter
+                    className='mx-auto d-block'
+                    style={{
+                        fontSize: 14,
+                        marginTop: 20
+                    }}>
+                    Is this your product ?</CardFooter>
+
+                <div style={{ marginTop: -5 }}>
+                    <Button className={Styles.button_1}>No</Button>
+                    <Button className={Styles.button_2}>Yes</Button>
+                </div>
+            </Card>
+        </div>
+    )
+}
+
+export default ResultCard
