f69b15bc7fbd51bbec2950e00e07b9a955962aab
vihanpereraux <vihanpereraux@gmail.com>
2025-01-12 08:31:43 +0000

    add basic skelton

--- app/Chat/page.tsx
+++ app/Chat/page.tsx
@@ -1,21 +1,40 @@
 "use client"
-
 import React, { useEffect, useState } from "react"
-import { useRouter } from "next/router"
+
+// ui components
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 // interfaces
 import { ChatScreenProps } from "@/interfaces/Result-Card-Interface"
 
+// stylesheet
+import Styles from './Styles.module.css'
+
 const Chat: React.FC<ChatScreenProps> = ({ reviews }) => {
     // const [data, setData] = useState<String []>('')
-    
+
     useEffect(() => {
 
     }, [])
-    
+
     return (
         <>
-            <div style={{ color: "white" }}>Chat Screen</div>
+            <div className={Styles.chat_screen_wrapper}>
+                {/* chat box */}
+                <div className={Styles.chat_display}>
+
+                </div>
+
+                {/* input */}
+                <div className={Styles.input}>
+                    <Input 
+                        className={Styles.input_field} 
+                        type="text" 
+                        placeholder="Type your questions here ..." />
+                    <Button className={Styles.btn}>Ask</Button>
+                </div>
+            </div>
         </>
     )
 }
